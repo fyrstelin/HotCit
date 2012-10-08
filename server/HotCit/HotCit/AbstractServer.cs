6f28382987680dcf024afa0160048ac2bb339582
Andreas Fyrstelin Kristiansen <AFKpost@gmail.com>
2012-10-08 09:31:24 +0200

    LobbyServer

---
+++ server/HotCit/HotCit/AbstractServer.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace HotCit
+{
+    public abstract class AbstractServer<T> : RestServiceBase<T>
+    {
+        protected readonly GameFactoryRepository FactoryRepository = GameFactoryRepository.GetInstance();
+        protected readonly GameRepository GameRepository = GameRepository.GetInstance();
+
+        protected string User
+        {
+            get
+            {
+                var user = Request.Headers[HttpHeaders.Authorization];
+                if (user == null) throw new HttpError(HttpStatusCode.Unauthorized, "Please provide credentials");
+                return user;
+            }
+        }
+
+        protected UserGameFactory GetFactory(string id)
+        {
+            var fac = FactoryRepository.GetFactory(id);
+            if (fac == null) throw new HttpError(HttpStatusCode.NotFound, "Game " + id + " not found");
+            return fac;
+        }
+
+    }
+}
---
+++ server/HotCit/HotCit/Requests.cs
@@ -0,0 +1,20 @@
+﻿namespace HotCit
+{
+    public class LobbyRequest
+    {
+        public string GameId { get; set; }
+        public string Password { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+    }
+
+    public class JoinRequest
+    {
+        public string GameId { get; set; }
+    }
+
+    public class ReadyRequest
+    {
+        public string GameId { get; set; }
+    }
+}
