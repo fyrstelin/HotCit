92ab577c6ef0aa4cf7c4eaf2dcb0df80afd244f2
Andreas Fyrstelin Kristiansen <AFKpost@gmail.com>
2012-11-01 14:01:12 +0100

    dlls included

--- server/HotCit/HotCit/Global.asax.cs
+++ server/HotCit/HotCit/Global.asax.cs
@@ -19,8 +19,8 @@ namespace HotCit
             public override void Configure(Container container)
             {
                 //This may not be neccesary since repositories are using singleton pattern
-                container.RegisterAutoWired<LobbyServer>();
-                /*container.RegisterAutoWired<JoinServer>();
+                /*container.RegisterAutoWired<LobbyServer>();
+                container.RegisterAutoWired<JoinServer>();
                 container.RegisterAutoWired<ReadyServer>();
                 container.RegisterAutoWired<GameServer>();
                 container.RegisterAutoWired<ResourceServer>();*/
