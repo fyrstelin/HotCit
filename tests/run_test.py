f7cdcc3bca47a3aeae8510d197b433193aec3833
Mads <tugend@gmail.com>
2012-11-05 21:17:05 +0100

    stuff works
    try to run example.test, and gamecreate.test,
    gamecreate currently fails on a server bug

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,25 +1,17 @@
 import httplib, urllib, json, base64
-import httplib
-
-import unittest
 from test_parser import parse
-from logging import DEBUG
+import sys
+from sys import argv
 
 content_type = 'application/json'
 accept = 'application/json'
 
-
-def send_request(server, method, username, url, params, debug):
+def send_request(server, method, url, params, headers, debug=False):
     #auth_type = 'Basic'
     #password = 'secret'
     #auth_hash = username + ':' + password
     #user_info = base64.b64encode(auth_hash)
 
-    #set headers
-    headers = {}
-    if content_type: headers['Content-Type'] = content_type
-    if username: headers['Authorization'] = username
-    if accept: headers['Accept'] = accept
 
     #send request
     conn = httplib.HTTPConnection(server)
@@ -27,7 +19,10 @@ def send_request(server, method, username, url, params, debug):
     if debug:
         conn.set_debuglevel(DEBUG)
 
-    conn.request(method, url, json.dumps(params), headers)
+    if params:
+        params = json.dumps(params)
+        
+    conn.request(method, url, params, headers)
     res = conn.getresponse()
     conn.close()
 
@@ -36,41 +31,102 @@ def send_request(server, method, username, url, params, debug):
     reason = res.reason
     data = res.read()
 
-
     return (status, reason, data)
 
 
 class AssertExc(Exception):
     pass
 
-
+import re, ast
 def assertEquals(value, expected, key):
-    if str(value).lower().strip() != str(expected).lower().strip():
-        msg = "  expected %s to be \r\n\t> %s <\r\n\t  and not \r\n\t> %s <" % (key, expected, value)
+    value = str(value).lower()
+    expected = str(expected).lower()
+       
+    if value != expected:
+        msg = "\n"
+        msg += key[0].upper()+key[1:] + ':'
+        msg += "\n\t"
+        msg += "Expected".ljust(12) + ": %s" % expected
+        msg += "\n\t"
+        msg += "Actual".ljust(12) + ": %s" % value
         raise AssertExc(msg)
     
 
 def run_test(testno, debug, server, description=None, username=None, password=None, url=None, reason=None, params=None, method='GET', statuscode=200, data=None):   
 
-    (rstatus, rreason, rdata) = send_request(server, method, username, url, params, debug)
+    #set headers
+    headers = {}
+    headers['Content-Type'] = content_type
+    headers['Accept'] = accept
+    if username: headers['Authorization'] = username
     
+    (rstatus, rreason, rdata) = send_request(server, method, url, params, headers, debug)
+    
+    if data:
+         # hack to get rid of keys encoded in unicode =/
+         # and to get all keys sorted in the same order
+         # and to get rid of all sugar spaces
+
+        try:
+            data = json.dumps(data)
+        except:
+            pass
+            
+    if rdata:
+        try:
+            rdata = json.dumps(json.loads(rdata))
+        except:
+            pass
+        
     print 'TEST', testno, ':', description.ljust(20),':', 
     try:
-        if statuscode:  assertEquals(rstatus, statuscode, 'statuscode')
+        if statuscode:  assertEquals(int(rstatus), int(statuscode), 'statuscode')
         if reason:      assertEquals(rreason, reason, 'reason')
         if data:        assertEquals(rdata, data, 'data')
         print '\tCHECK'
         return True
     
     except AssertExc as e:
-        print '\tERROR'
-        print e.message
+        response = ''
+        response += '\tERROR' + '\n'
+        response += e.message + '\n'
+        response += '\n'
+        response += 'Uri: %s' % method + '  ' + server+url + '\n'
+        response += '\n'
+        if headers:
+            response += 'Headers:' + '\n'
+            for key, value in headers.iteritems():
+                response += '\t' + key + ': ' + value + '\n'
+        response += '\n'
+        if params:
+            response += 'Params:' + '\n'
+            for key, value in params.iteritems():
+                response += '\t' + key + ':' + json.dumps(value) + '\n'
+        
+        response += '\n'
+        if rdata:
+            response += 'Content body:' + '\n'
+            try:
+                rdata = json.loads(rdata)
+            except:
+                pass
+                
+            if isinstance(rdata,dict):
+                for key, value in rdata.iteritems():
+                    response += '\t' + key + ':' + json.dumps(value) + '\n'
+            else:
+                response += '\t' + rdata
+        
+        print response
+        with open('error.txt', 'w') as f:
+            f.write(response)
+        print
+        print 'error written to error.txt'
         return False
 
             
 if __name__ == '__main__':
-    import sys
-    from sys import argv
+
     
     if len(argv) < 2:
         print 'you\'re doing it wrong sir. I need a file to parse as input...'
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,63 +1,77 @@
 import os, re
 import json
 
-# http://jsonlint.com/
-def parse(path):
-    '''
-    if file contains a valid JSON list, except "",
-    parse(filepath) returns an ordered list of keyword argument
-    dicts using the first line in input list as keyword names,
-    and the following as keyword values.
+def extractServer(lines):
+    server, lines = lines[0].strip(), lines[1:]
+    return server, lines
+  
+def extractKeysNames(lines):
+    valid_json_lists = []
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if line and not line.startswith('%'):
+            # first line that is not empty or a comment, should be keyword names
+            keys = _prune_sp_tokens(line)
+            keys = _commentify(keys)
+            keys = _prune_endspaces(keys)
 
-    everything to the right of the comment delim iter '%' is ignored
-
-    the order of the columns is not significant
-
-	www.google.dk
-	[
-	[description,         	method,     url,					statuscode],
-	%-----------------------------------------------------------------------------------------------------------------------------------------------
-	[GET GOOGLE TITTTIES, 	GET, 		/search?q=titties, 		200],
-	[POST GOOGLE TITTTIES, 	POST, 		search?q=titties, 		411]
-	]	
-
-    '''
-   
-    decoder = json.JSONDecoder()
-
-    with open(path, 'rb') as f:
-        raws = f.readlines()
-
-    server, raws = raws[0].strip(), raws[1:]
-    fraw = _prune_comments(raws)
-    fraw = _prune_sp_tokens(fraw)
-    valid_json = _commentify(fraw)
-
-    try:
-        json_list = decoder.decode(valid_json)
-        kwargs = _extract_kwargs(json_list)
-    except Exception as e:
-        print 'hmm. sure this is valid json? Try looking in error.txt.'
-        with open('error.txt', 'w') as f:
-            f.write(valid_json)
-        print
-        raise
-    return server, kwargs
-
-
-def _extract_kwargs(json_list):
-    kwarg_names = json_list[0]
-
+            try:
+                keys = json.loads(keys)
+                return keys, lines[i+1:]
+            except ValueError:
+                with open('error.txt', 'w') as f:
+                    f.write(keys)
+                print 'It seems the keywords could not be parsed as json, have a look at error.txt'
+                print
+                raise
+                
+    print 'could not find any keyword names'
+    sys.exit(1)
+      
+      
+def _extract_kwargs(test_values, keys):
     res = []
-    for test in json_list[1:]:
-        kwargs = {}
-        for i, value in enumerate(test):
-            if not value: None
-            kwargs[kwarg_names[i].strip().replace(' ', '_')] = value
-
-        res.append(kwargs)
-    return res
-
+    kwargs = {}
+    for i, value in enumerate(test_values):
+        try:
+            kwargs[keys[i].strip().replace(' ', '_')] = value
+        except:
+            print 'Something went wrong in combining a key with a value'
+            print 'Might there be more values, than keys?'
+            print 'I am looking for the', i, 'th key in', keys
+            print 'and want to combine it with the value', value
+            print
+            raise
+            
+    return kwargs  
+  
+ 
+def extractTests(lines, keys):
+    valid_json_lists = []
+    for i, line in enumerate(lines):
+        if line.strip().startswith('%') or not line.strip():
+            continue
+            
+        valid_json = ''
+        line = _prune_comments(line)
+        line = _prune_sp_tokens(line)
+        valid_json = _commentify(line)
+        valid_json = _prune_endspaces(valid_json)
+        
+        try:
+            values = json.loads(valid_json) # check to see if it parses as valid json
+            test = _extract_kwargs(values, keys)
+            valid_json_lists.append(test)
+            
+        except ValueError:
+            print 'Something is wrong with the', i, 'testcase, it seems it is not valid json.'
+            print 'have a look at error.txt'
+            with open('error.txt', 'w') as f:
+                f.write(valid_json)
+            print
+            raise
+    return valid_json_lists    
+    
 def _prune_comments(raws):
     raw = ''
     for line in raws:
@@ -71,7 +85,7 @@ def _prune_comments(raws):
 
 
 def _prune_sp_tokens(fraw):
-    return re.sub('[\t\s]+',' ',fraw)
+    return re.sub('[\t\s]+', ' ', fraw)
 
 
 def _commentify(raw):
@@ -137,7 +151,41 @@ def _commentify(raw):
         if char != ' ':
             last_char = new_last_char
             
-    return formatted_raw.replace('@','')
+    return formatted_raw.replace('@','')   
 
+def _prune_endspaces(raw):
+    return re.sub('\s*",','",', raw) 
+                    
+# http://jsonlint.com/
+def parse(path):
+    '''
+    if file contains a valid JSON list, except "",
+    parse(filepath) returns an ordered list of keyword argument
+    dicts using the first line in input list as keyword names,
+    and the following as keyword values.
+
+    everything to the right of the comment delim iter '%' is ignored
+
+    the order of the columns is not significant
+
+	www.google.dk
+	[
+	[description,         	method,     url,					statuscode],
+	%-----------------------------------------------------------------------------------------------------------------------------------------------
+	[GET GOOGLE TITTTIES, 	GET, 		/search?q=titties, 		200],
+	[POST GOOGLE TITTTIES, 	POST, 		search?q=titties, 		411]
+	]	
+
+    '''
+
+    with open(path, 'rb') as f:
+        lines = f.readlines()
+    
+    server, lines = extractServer(lines)
+    keys, lines = extractKeysNames(lines)
+    tests = extractTests(lines, keys)
+    return server, tests
+
+    
 if __name__ == '__main__':
     print parse('example.test')
