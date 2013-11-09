8e1f07b76ba513feb8a88cb87e317ce712c107f6
Andreas Kristiansen <afkpost@gmail.com>
2013-11-09 19:39:48 +0100

    Client mocked, simple views, mustache introduced

--- client/web/js/game.js
+++ client/web/js/game.js
@@ -2,12 +2,33 @@
 require.config({
 	baseUrl: "js",
 	paths : {
-		jquery : "libs/jquery-2.0.3"
+		jquery : "libs/jquery-2.0.3",
+		mustache: "libs/mustache"
 	}
 });
 
-require(["jquery", "model"], function ($, model) {
-  model.getHand(function(hand) {
-    console.log(hand);
-  });
+require(["jquery", "model", "mustache"], function ($, model, Mustache) {
+	"use strict";
+	var elm = {
+		player: $('#player'),
+		players: $('#players')
+	}, templates = {
+		each: function (es, t) { //maybe part of mustache?
+			var res = "";
+			$.each(es, function (i, e) {
+				res += t(e);
+			});
+			return res;
+		},
+		player: Mustache.compile($('#playerTemplate').html())
+	};
+
+	model.getMe(function (player) {
+		elm.player.html(templates.player(player));
+	});
+
+
+	model.getOtherPlayers(function (players) {
+		elm.players.html(templates.each(players, templates.player));
+	});
 });
--- client/web/js/model.js
+++ client/web/js/model.js
@@ -1,17 +1,47 @@
 /*global define*/
 
 define("model", function () {
-  "use strict";
-  var mock = {
-    hand: ["cathedral",
-           "dragon_gate",
-           "great_wall",
-           "watchtower"]
-  };
+	"use strict";
+	var mock = {
+		me: {
+			Username: "Nygaard",
+			City: ["manor", "battlefield", "university", "monastery", "school_of_magic"],
+			Hand: ["cathedral", "dragon_gate", "great_wall", "watchtower"],
+			Points: 3,
+			Gold: 3,
+			Characters: ["king"]
+		},
+		players: [{
+			Username: "Turing",
+			City: [],
+			Hand: ["unknown_district", "unknown_district", "unknown_district", "unknown_district", "unknown_district"],
+			Points: 5,
+			Gold: 2,
+			Characters: ["thief"]
+		}, {
+			Username: "Ada",
+			City: [],
+			Hand: ["unknown_district", "unknown_district", "unknown_district", "unknown_district"],
+			Points: 3,
+			Gold: 1,
+			Characters: ["magician"]
+		}, {
+			Username: "Wiener",
+			City: [],
+			Hand: ["unknown_district"],
+			Points: 5,
+			Gold: 6,
+			Characters: ["unknown_character"]
+		}]
+	};
 
-  return {
-    getHand: function (done) {
-      done(mock.hand);
-    }
-  }
+	return {
+		getMe: function (done) {
+			done(mock.me);
+		},
+
+		getOtherPlayers: function (done) {
+			done(mock.players);
+		}
+	};
 });
