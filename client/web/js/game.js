c7994a6ee3e3f9227be4c43b733cad5cbce8184b
Andreas Kristiansen <afkpost@gmail.com>
2013-11-11 02:27:25 +0100

    JQuery UI included
    May be needed for draggable cards, popups etc

--- client/web/js/game.js
+++ client/web/js/game.js
@@ -2,33 +2,36 @@
 require.config({
 	baseUrl: "js",
 	paths : {
-		jquery : "libs/jquery-2.0.3",
+		jquery :  "libs/jquery-2.0.3",
+		jquiryui: "libs/jquery-ui",
 		mustache: "libs/mustache"
 	}
 });
 
-require(["jquery", "model", "mustache"], function ($, model, Mustache) {
+require(["jquery", "model", "mustache", "jquiryui"], function ($, model, Mustache) {
 	"use strict";
-	var elm = {
-		player: $('#player'),
-		players: $('#players')
-	}, templates = {
-		each: function (es, t) { //maybe part of mustache?
-			var res = "";
-			$.each(es, function (i, e) {
-				res += t(e);
-			});
-			return res;
-		},
-		player: Mustache.compile($('#playerTemplate').html())
-	};
+	$(function() {
+		var elm = {
+			player: $('#player'),
+			players: $('#players')
+		}, templates = {
+			each: function (es, t) { //maybe part of mustache?
+				var res = "";
+				$.each(es, function (i, e) {
+					res += t(e);
+				});
+				return res;
+			},
+			player: Mustache.compile($('#playerTemplate').html())
+		};
 
-	model.getMe(function (player) {
-		elm.player.html(templates.player(player));
-	});
+		model.getMe(function (player) {
+			elm.player.html(templates.player(player));
+		});
 
 
-	model.getOtherPlayers(function (players) {
-		elm.players.html(templates.each(players, templates.player));
+		model.getOtherPlayers(function (players) {
+			elm.players.html(templates.each(players, templates.player));
+		});
 	});
 });
