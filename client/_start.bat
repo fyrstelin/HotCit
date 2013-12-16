4fc35ab71b9b565113e6350b33aa2b3f2c2a29eb
Mads <tugend@gmail.com>
2013-12-16 15:27:17 +0100

    updated _start.bat
    new: script do not exit when error occur on sass compilation
    
    new: disabled cache as the feature uses an absolute path which cannot
    handle ../Users/Wølch/..

--- client/_start.bat
+++ client/_start.bat
@@ -1,2 +1,13 @@
+:: start server
 start HotCit.exe
-sass --watch web/css/sass:web/css
+
+:: compile all css
+:: use no-cache required for mads' pc
+call sass --no-cache --watch web/css/sass:web/css 
+:: pause, instead of exit if error occur
+pause
+
+:: open game in chrome browser
+cd web
+start chrome http://localhost:8080/web/game.html
+cd ..
